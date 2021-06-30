c5c8fb1d0d477eb25e77f317a44bb2800f127c53
Ernest-Epistola <ernestepistola@gmail.com>
2021-07-01 07:50:57 +0800

    Moved the ApplicationState class to state_management.dart

--- lib/application/src/state_management/state_management.dart
+++ lib/application/src/state_management/state_management.dart
@@ -1,5 +1,21 @@
 library state_management;
 
-export 'application_state.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
 export 'flows/flows.dart';
 export 'mixins/mixins.dart';
+
+abstract class ApplicationState extends ChangeNotifier {
+  bool get isAppAuthenticated;
+}
+
+class AppStateProvider extends ChangeNotifierProvider<ApplicationState> {
+  AppStateProvider(
+    ApplicationState appStateModel, {
+    Widget? child,
+  }) : super(
+          create: (_) => appStateModel,
+          child: child,
+        );
+}

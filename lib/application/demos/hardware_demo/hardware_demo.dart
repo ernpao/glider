f7c343dd4ded9b971ada564b356b891c5190f2c3
Ernest-Epistola <ernestepistola@gmail.com>
2021-07-05 23:48:38 +0800

    Initial implementation of SensorManager and SensorListener classes.

--- lib/application/demos/hardware_demo/hardware_demo.dart
+++ lib/application/demos/hardware_demo/hardware_demo.dart
@@ -2,7 +2,23 @@ import 'package:flutter/material.dart';
 
 import '../../src/application_layer.dart';
 
-class HardwareDemo extends StatelessWidget {
+class HardwareDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HardwareDemoState();
+}
+
+class _HardwareDemoState extends State<HardwareDemo> {
+  late AccelerometerListener accel;
+
+  late GyroscopeListener gyro;
+
+  @override
+  void initState() {
+    accel = new AccelerometerListener(onSensorEvent: print);
+    gyro = new GyroscopeListener(onSensorEvent: print);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Application(
--- lib/application/src/hardware/hardware.dart
+++ lib/application/src/hardware/hardware.dart
@@ -1,6 +1,69 @@
 library hardware;
 
 export 'package:camera/camera.dart';
+import 'dart:async';
+
+import 'package:sensors_plus/sensors_plus.dart';
 
 /// Helper class for mobile phone hardware such as sensors, cameras, etc.
-class HardwareManager {}
+class SensorManager {
+  static final List<SensorListener> _sensorListeners = [];
+  // static final List<StreamSubscription> _subsciptions = [];
+
+  static Stream<AccelerometerEvent> get _accelerometer => accelerometerEvents;
+
+  static Stream<GyroscopeEvent> get _gyroscope => gyroscopeEvents;
+
+  static bool _sensorsActive = false;
+  static bool get _sensorsInactive => !_sensorsActive;
+
+  static _addListener(SensorListener listener) {
+    _sensorListeners.add(listener);
+    if (_sensorsInactive) _initializeSensors();
+  }
+
+  static void _initializeSensors() {
+    _accelerometer.listen((e) => _process<AccelerometerEvent>(e));
+    _gyroscope.listen((e) => _process<GyroscopeEvent>(e));
+    _sensorsActive = true;
+  }
+
+  static bool _isListening(SensorListener listener) =>
+      _sensorListeners.contains(listener);
+
+  static void _process<T>(e) {
+    _sensorListeners
+        .where((l) => l.eventType == e.runtimeType)
+        .forEach((l) => (l as SensorListener<T>).process(e));
+  }
+
+  static void _removeListener(SensorListener listener) {
+    _sensorListeners.remove(listener);
+  }
+}
+
+abstract class SensorListener<T> {
+  SensorListener({required this.process}) {
+    assert(eventType == GyroscopeEvent || eventType == AccelerometerEvent);
+    listen();
+  }
+
+  Type get eventType => T;
+  final void Function(T event) process;
+
+  void listen() => SensorManager._addListener(this);
+  void stopListening() => SensorManager._removeListener(this);
+  void toggleListen() =>
+      SensorManager._isListening(this) ? stopListening() : listen();
+}
+
+class GyroscopeListener extends SensorListener<GyroscopeEvent> {
+  GyroscopeListener({required void Function(GyroscopeEvent) onSensorEvent})
+      : super(process: onSensorEvent);
+}
+
+class AccelerometerListener extends SensorListener<AccelerometerEvent> {
+  AccelerometerListener(
+      {required void Function(AccelerometerEvent) onSensorEvent})
+      : super(process: onSensorEvent);
+}

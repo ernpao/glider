b1cd6ad7effda514eba1f26f8dc0c30970378a9d
Ernest-Epistola <ernestepistola@gmail.com>
2021-07-10 20:50:37 +0800

    Removed the export statements for hover.

--- lib/glider.dart
+++ lib/glider.dart
@@ -1,8 +1,6 @@
 library glider;
 
 export 'package:glider_models/glider_models.dart';
-export 'package:hover/framework.dart';
-export 'package:hover/widgets.dart';
 
 export 'src/application/application_layer.dart';
 export 'src/data/data_layer.dart';

dfe9c0fb9a7062c89bd1fc4603a5bcbfefd48d5a
Ernest-Epistola <ernestepistola@gmail.com>
2021-08-05 19:28:18 +0800

    Removed android/ios/web build folders for library packages.

--- glider_keynote/android/app/src/main/kotlin/com/example/glider_keynote/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package tech.codera.glider.keynote
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- glider_models/android/app/src/main/kotlin/com/example/glider_models/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package tech.codera.glider.models
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}

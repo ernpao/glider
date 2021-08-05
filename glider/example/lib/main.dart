3052b1cf1c95cb9f1554f8d3e0254c8164059d62
Ernest-Epistola <ernestepistola@gmail.com>
2021-08-05 19:23:56 +0800

    Created a separate project for the glider_demo example app.

--- glider/example/lib/main.dart
+++ glider/example/lib/main.dart
@@ -17,5 +17,5 @@ final CustomWidgetsDemo customWidgetsDemo = CustomWidgetsDemo(
 final MachineLearningDemo machineLearningDemo = MachineLearningDemo();
 
 void main() {
-  runApp(customWidgetsDemo);
+  runApp(machineLearningDemo);
 }
